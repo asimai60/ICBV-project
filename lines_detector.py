f06d6e5f21e3e40915fe893810f1b05e177afe3e
Eshdat <112461718+Eshdat@users.noreply.github.com>
2024-04-28 19:03:24 +0300

    Add files via upload

--- lines_detector.py
+++ lines_detector.py
@@ -3,6 +3,9 @@ import cv2
 import os
 import matplotlib.pyplot as plt
 import time
+from scipy.signal import fftconvolve
+from scipy.ndimage import uniform_filter
+from skimage.draw import circle_perimeter
 
 plastic_path = r"C:\Users\nrhot\Downloads\WhatsApp Unknown 2024-04-24 at 12.53.26\cropped\PLAST"
 glass_path = r'C:\Users\nrhot\Downloads\WhatsApp Unknown 2024-04-24 at 12.53.26\cropped\GLASS'
@@ -13,6 +16,11 @@ RHO = 1
 THETA = np.pi/ 45
 LINESTH = 75
 
+Local_max_Th = 0.6
+LOW2_threshold = 30
+HIGH2_threshold = 150
+bin_size = 3
+K_size = (5, 5)
 
 
 def plot_circles(circles, image):
@@ -26,11 +34,6 @@ def plot_circles(circles, image):
         cv2.circle(output, (x, y), r, (0, 255, 0), 4)
         # Draw a rectangle (center point) in the output image
         cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
-    # Display the result in a matplotlib window
-    plt.imshow(cv2.cvtColor(output, cv2.COLOR_BGR2RGB))
-    plt.title('Detected Circles')
-    plt.axis('off')
-    plt.show()
 
 
 def load_and_resize(path):
@@ -38,27 +41,6 @@ def load_and_resize(path):
     desired_shape = (480, 480)
     image = cv2.resize(image, desired_shape)
     return image
-
-
-def feature_extraction(image):
-    # Convert gray image to uint8
-    gray_image = (image * 255).astype(np.uint8)
-    glcm = feature.graycomatrix(gray_image, [1], [0, np.pi / 4, np.pi / 2, 3 * np.pi / 4], levels=256, symmetric=True,
-                                normed=True)
-
-    # Extract properties from GLCM
-    contrast = feature.graycoprops(glcm, 'contrast')
-    dissimilarity = feature.graycoprops(glcm, 'dissimilarity')
-    homogeneity = feature.graycoprops(glcm, 'homogeneity')
-    energy = feature.graycoprops(glcm, 'energy')
-    correlation = feature.graycoprops(glcm, 'correlation')
-    print(f'Contrast: {contrast}')
-    print(f'Dissimilarity: {dissimilarity}')
-    print(f'Homogeneity: {homogeneity}')
-    print(f'Energy: {energy}')
-    print(f'Correlation: {correlation}')
-    data = [contrast, dissimilarity, homogeneity, energy, correlation]
-    return data
 
 
 def canny(gray):
@@ -121,11 +103,11 @@ def detect_lines(image):
     y_start = int(h * crop_size)
     y_end = int(h * (1 - crop_size))
 
-    # Crop the image 
+    # Crop the image
     gray_cropped = gray[y_start:y_end, x_start:x_end]
     # Apply edge detection using the Canny edge detector
     edges = canny(gray_cropped)
-    edges_full = canny(gray)
+
 
     # Use HoughLines to detect lines in the edge map
     lines = cv2.HoughLines(edges, RHO, THETA, LINESTH)  # These parameters may need adjustment for your specific case
@@ -176,29 +158,93 @@ def detect_lines(image):
         plt.show()"""
         return True
     else:
-        print("no lines detected")
-        # Show the result
-
-        # hough_circles = cv2.HoughCircles(edges_full, cv2.HOUGH_GRADIENT, 1, 80, param1=50, param2=50, minRadius=100, maxRadius=0)
-        # if hough_circles is not None:
-        #     plot_circles(hough_circles, image)
-        # else:
-        #     print('No circles detected!')
-        plt.figure(figsize=(10, 6))
-        plt.subplot(121)
-        plt.imshow(cv2.cvtColor(edges, cv2.COLOR_BGR2RGB))
-        # plt.imshow(cv2.cvtColor(edges_full, cv2.COLOR_BGR2RGB))
-        plt.title('Original Image')
-        plt.axis('off')
-
-        plt.subplot(122)
-        plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-        plt.title('Detected Lines')
-        plt.axis('off')
-
-        plt.show()
         return False
 
+
+def canny2(gray):
+    # Apply Gaussian Blurring to reduce noise and improve edge detection
+    center = gray.shape[0]//2
+    inner_circle = cv2.circle(gray, (center, center), center - gray.shape[0]//8, (0, 0, 0),-1)
+    blurred = cv2.GaussianBlur(inner_circle, K_size, 0)
+    # Use adaptive thresholding to create a binary image
+    adaptive_thresh = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                                            cv2.THRESH_BINARY, 11, 2)
+    edges = cv2.Canny(adaptive_thresh, LOW2_threshold, HIGH2_threshold)
+    return edges
+
+
+def HoughCircles(image, path):
+    # Set the radius, cx and cy range
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    edge_map = canny2(gray)
+    height, width = edge_map.shape
+    max_radius = width // 2
+
+    accumulator = generate_accumulator(edge_map, max_radius, bin_size)
+
+    local_maxima = find_local_maxima(accumulator, Local_max_Th)
+    # Define the value you want to remove
+    value_to_remove = np.array([79, 81, 81])
+
+    # Create a condition where the second and third columns are not both 81
+    mask = ~(local_maxima[:, 1] == 81) | ~(local_maxima[:, 2] == 81)
+
+    # Apply the mask to 'local_maxima' to filter out the unwanted row
+    local_maxima = local_maxima[mask]
+
+    return local_maxima
+
+
+def generate_accumulator(edge_map, max_radius, bin_size=1):
+    # Use uniform_filter to effectively "bin" the image
+    # by computing the local mean over the areas of size (bin_size x bin_size)
+    # This is much more efficient than manually summing up the pixels
+    if bin_size > 1:
+        edge_map = uniform_filter(edge_map, size=bin_size)
+        edge_map = edge_map[::bin_size, ::bin_size]
+
+    # Initialize the accumulator array for the binned edge map
+    accumulator = np.zeros((max_radius, *edge_map.shape))
+
+    # Create a two-dimensional grid of coordinates for the binned edge map
+    y, x = np.indices(edge_map.shape)
+    max_radius_binned = max_radius // bin_size + 1
+    # Generate circle masks and accumulate
+    for radius in range(1, max_radius_binned):
+        # Generate the mask for this radius
+        mask = np.zeros_like(edge_map, dtype=float)
+        rr, cc = circle_perimeter(edge_map.shape[0]//2, edge_map.shape[1]//2, radius)
+
+        # Ensure that the indices are within the dimensions of 'mask'
+        rr = np.clip(rr, 0, mask.shape[0] - 1)
+        cc = np.clip(cc, 0, mask.shape[1] - 1)
+
+        mask[rr, cc] = 1
+
+        # Convolve and accumulate the results in the corresponding layer of the accumulator
+        accumulator[radius - 1] = fftconvolve(edge_map, mask, mode='same')
+
+    return accumulator
+
+
+def find_local_maxima(accumulator, threshold=0.5, neighborhood_size=3):
+    threshold_abs = threshold * np.max(accumulator)
+    local_maxima = np.zeros_like(accumulator, dtype=bool)
+
+    # Iterate over each pixel in the accumulator
+    for index, value in np.ndenumerate(accumulator):
+        if value >= threshold_abs:
+            # Define the neighborhood boundaries
+            min_bound = np.maximum(np.subtract(index, (neighborhood_size // 2,)*len(index)), 0)
+            max_bound = np.minimum(np.add(index, (neighborhood_size // 2 + 1,)*len(index)), accumulator.shape)
+
+            neighborhood = accumulator[min_bound[0]:max_bound[0], min_bound[1]:max_bound[1]]
+
+            # Check if the current pixel is the maximum within its neighborhood
+            if value == np.max(neighborhood):
+                local_maxima[index] = True
+    peaks_indices = np.argwhere(local_maxima)
+    return peaks_indices
 
 
 def main(path, label):
@@ -212,33 +258,41 @@ def main(path, label):
         image_path = os.path.join(path, image_file)
         # Read the image
         image = load_and_resize(image_path)
-        gray_masked = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        shape = gray_masked.shape[0]
         lines = detect_lines(image)
         if not lines:
-            if label == "glass":
-                correct = correct + 1
+            circls = HoughCircles(image,image_path)
+            if circls.any():
+                print("found_circles")
+                if label == "glass":
+                    correct = correct + 1
+                else:
+                    print(image_path[70:])
+            else:
+                if label == "plastic":
+                    correct = correct + 1
+                else:
+                    print(image_path[70:])
+
             not_detected += 1
 
         else:
             if label == "plastic":
                 correct = correct + 1
-        time.sleep(2)
+            else:
+                print(image_path[70:])
+    time.sleep(2)
 
-    return not_detected / amuont * 100, correct
+    return amuont, correct
 
 
 
-print("plastic data:")
-plastic, prate = main(plastic_path, "plastic")
 print("glass data")
-glass , pglass= main(glass_path, "glass")
-
-print("plastic:" ,plastic)
-print("glass:", glass)
+amount_g , pglass= main(glass_path, "glass")
+print("plastic data:")
+amount_p, prate = main(plastic_path, "plastic")
 print("prate:", prate)
 print("pglass:", pglass)
-print("succses rate", (prate + pglass) / 35 *100)
+print("succses rate", (prate + pglass) / (amount_g + amount_p) * 100)
 
 
 
